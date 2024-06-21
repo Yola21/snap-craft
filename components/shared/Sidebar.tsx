918f31299fd8eae81706a0c2f54aacaf08e7b315
Yola21 <yashkhorja4@gmail.com>
2024-06-21 18:59:15 -0300

    modified sidebar

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -13,10 +13,6 @@ const Sidebar = () => {
   return (
     <aside className="sidebar">
       <div className="flex size-full flex-col gap-4">
-        <Link href="/" className="sidebar-logo">
-          <Image src="/assets/images/logo-text.svg" alt="logo" width={180} height={28} />
-        </Link>
-
         <nav className="sidebar-nav">
           <SignedIn>
             <ul className="sidebar-nav_elements">
